3c949895fd8d0b923562096b90ed17f20c9700d2
JosiahFu <josiahfu@gmail.com>
2022-10-15 22:04:13 -0700

    Initial Bad Code

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import fs from 'fs';
+
+class Point {
+    x: number;
+    y: number;
+    static regex = /\((-?\d+(?:\.\d+)?), (-?\d+(?:\.\d+)?)\)/;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    static from(pointString: string) {
+        const results = this.regex.exec(pointString);
+        if (results != null) {
+            return new Point(parseFloat(results[1]), parseFloat(results[2]));
+        }
+        return new Point(0,0);
+    }
+    toString() {
+        return `(${this.x}, ${this.y})`;
+    }
+}
+
+const swap = <T>(a: T, b: T, condition: boolean): [T, T] => condition ? [b, a] : [a, b];
+
+const genEquation = (point1: Point, point2: Point, func: (a: number) => number, printFunc: (a: string) => string, inverted: boolean, opposite: boolean): string => {
+   let [[x1, y1], [x2, y2]] = swap(swap(point1.x, point1.y, inverted), swap(point2.x, point2.y, inverted), opposite); 
+   //y=a*f(x-h)+k
+   return `${inverted ? 'x' : 'y'}=${(y2-y1) / func(x2-x1)}${printFunc(`${inverted ? 'y' : 'x'}-${x1}`)}+${y1}\\left\\{${Math.min(x1,x2)}<${inverted ? 'y' : 'x'}<${Math.max(x1,x2)}\\right\\}`;
+};
+
+const calcLine = (point1: Point, point2: Point, type: string) => {
+    const opts = type.split(' ');
+    const command = opts.shift();
+    const inverted = opts.includes('-i');
+    const opposite = opts.includes('-o');
+    switch(command) {
+        case 'Linear':
+            if (point1.x == point2.x) {
+                return `x=${point1.x}\\left\\{${Math.min(point1.y,point2.y)}<y<${Math.max(point1.y,point2.y)}\\right\\}`;
+            } else if (point1.y == point2.y) {
+                return `y=${point1.y}\\left\\{${Math.min(point1.x,point2.x)}<x<${Math.max(point1.x,point2.x)}\\right\\}`;
+            } else{
+                const m = (point1.y - point2.y) / (point1.x - point2.x);
+                return `y=${m}x+${point1.y - m*point1.x}\\left\\{${Math.min(point1.x,point2.x)}<x<${Math.max(point1.x,point2.x)}\\right\\}`;
+            }
+        case 'Quadratic':
+            return genEquation(point1, point2, (a) => Math.pow(a, 2), (a) => `\\left(${a}\\right)^{2}`, inverted, opposite);
+        case 'Cubic':
+            return genEquation(point1, point2, (a) => Math.pow(a, 3), (a) => `\\left(${a}\\right)^{3}`, inverted, opposite);
+        default:
+            return '';
+    }
+}
+
+fs.readFile('./points.txt', 'utf8', (err: any, data: string) => {
+    let text_expressions = data.split('\n\n');
+    let expressions = text_expressions.map(i => (i.match(/.*(?= @)/) ?? [i])[0]).filter(i => i != null);
+    for (let i = 0; i < expressions.length; i++) {
+        if(!Point.regex.test(expressions[i])) continue;
+        if(!Point.regex.test(expressions[i+2])) continue;
+        if (!/".*"/.test(expressions[i+1])) continue;
+
+        let point1 = Point.from(expressions[i]);
+        let point2 = Point.from(expressions[i+2]);
+        let type = (/"(.*)"/.exec(expressions[i+1]) ?? [''] )[1];
+
+        //console.log(point1, type, point2);
+
+        console.log(calcLine(point1, point2, type));
+
+    }
+});
